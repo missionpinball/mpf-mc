78a5319addd8c33911ef6416393911de588ab3f5
toomanybrians <brian@missionpinball.com>
2016-01-16 00:43:00 -0800

    added tests for modes

---
+++ tests/test_Modes.py
@@ -0,0 +1,32 @@
+from .MpfMcTestCase import MpfMcTestCase
+
+
+class TestModes(MpfMcTestCase):
+    def get_machine_path(self):
+        return 'tests/machine_files/modes'
+
+    def get_config_file(self):
+        return 'test_modes.yaml'
+
+    def test_mc_mode_start(self):
+        # this tests that the mode is read properly from the config too
+        self.mc.bcp_processor._process_command(bcp_command='mode_start',
+                                               name='mode1')
+        self.assertTrue(self.mc.modes['mode1'].active)
+
+        # try to start the mode again and make sure it doesn't explode
+        self.mc.bcp_processor._process_command(bcp_command='mode_start',
+                                               name='mode1')
+
+        # stop the mode
+        self.mc.bcp_processor._process_command(bcp_command='mode_stop',
+                                               name='mode1')
+        self.assertFalse(self.mc.modes['mode1'].active)
+
+        # make sure the built-in modes are still there
+        self.mc.bcp_processor._process_command(bcp_command='mode_start',
+                                               name='attract')
+        self.assertTrue(self.mc.modes['attract'].active)
+
+        # also make sure this method doesn't explode
+        self.mc.mode_controller.dump()
