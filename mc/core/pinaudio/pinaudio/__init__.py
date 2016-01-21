6afc6239fe29e9bec434c635e1bc98e9f066c91f
Quinn Capen <qcapen@gmail.com>
2016-01-21 11:56:18 -0700

    Removed old PinAudio library

--- mc/core/pinaudio/pinaudio/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Pinaudio
-===========
-
-Pinaudio is a python extension that gives you a direct access to the speaker
-
-"""
-
-from pinaudio.core import *
--- mc/core/pinaudio/pinaudio/version.py
+++
@@ -1,2 +0,0 @@
-__version_info__ = ('0', '1', '0-dev3')
-__version__ = '.'.join(__version_info__)
