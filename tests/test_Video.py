0973769eb55f00b98046cc75a69b4a0e8086b995
toomanybrians <brian@missionpinball.com>
2016-01-27 14:32:32 -0800

    Changed test video since our travis config doesn't load QT libraries

--- tests/test_Video.py
+++ tests/test_Video.py
@@ -11,16 +11,20 @@ class TestVideo(MpfMcTestCase):
 
     def test_video(self):
 
+        # Note the green bar on the bottom of this video is part of it. It is
+        # VERY low quality to keep the mpf_mc package small. :)
         self.assertIn('mpf_video_small_test', self.mc.videos)
 
         self.mc.events.post('show_slide1')
-        self.advance_time(2)
+        self.advance_time(1)
 
         video_widget = self.mc.targets['default'].current_slide.children[0]
 
         self.assertEqual(video_widget.state, 'play')
-        self.assertAlmostEqual(video_widget.duration, 7.9, delta=.1)
+        self.assertTrue(video_widget.loaded)
+        self.assertAlmostEqual(video_widget.position, .8, delta=.3)
+        self.assertAlmostEqual(video_widget.duration, 7.79, delta=.1)
         self.assertEqual(video_widget.volume, 1.0)
-        self.assertEqual(video_widget.size, [398, 248])
+        self.assertEqual(video_widget.size, [400, 258])
 
         self.advance_time(1)
