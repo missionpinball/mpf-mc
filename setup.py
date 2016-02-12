01d9f58988689e0eafd21c345f75b33d8a462faa
toomanybrians <brian@missionpinball.com>
2016-02-11 21:43:25 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,85 @@
+"""Mission Pinball Framework Media Controller (mpf_mc) setup.py"""
+
+from platform import system
+from setuptools import setup, find_packages
+
+import version
+
+install_requires=['ruamel.yaml',
+                  'kivy.deps.sdl2',
+                  'kivy.deps.glew',
+                  'kivy.deps.gstreamer_dev',
+                  'mpf',
+                  'kivy',
+                  ]
+
+if system() == 'Windows':
+    install_requires.append('pypiwin32')
+
+setup(
+
+    name='mpf_mc',
+    version=version.__version__,
+    description='Mission Pinball Framework Media Controller',
+    long_description='''Graphics, video, and audio engine for the
+        Mission Pinball Framework.
+
+The Mission Pinball Framework (MPF) is an open source, cross-platform,
+Python-based software framework for powering real pinball machines.
+
+The Mission Pinball Framework Media Controller (MPF_MC, this package) is the
+component of MPF that controls graphics and sound, including DMDs,
+architecture is modular, with multiple options for driving graphics and
+sound. This MPF_MC package is one option, and there's another option based
+on Unity 3D.
+
+The MPF_MC is built on Kivy and leverages SDL2, OpenGL, and GPU-accelerated
+hardware.
+
+MPF is a work-in-progress that is not yet complete, though we're actively
+developing it and checking in several commits a week. It's MIT licensed,
+actively developed by fun people, and supported by a vibrant pinball-loving
+community.''',
+
+    url='https://missionpinball.com/mpf',
+    author='The Mission Pinball Framework Team',
+    author_email='brian@missionpinball.com',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Artistic Software',
+        'Topic :: Games/Entertainment :: Arcade'
+
+    ],
+
+    keywords='pinball',
+
+    include_package_data=True,
+
+    package_data={'': ['*.yaml',
+                       '*.png',
+                       '*.gif',
+                       '*.jpg',
+                       '*.zip'
+                       ]},
+
+    packages = find_packages(),
+
+    # zip_safe=True,
+
+    install_requires=install_requires,
+
+    # setup_requires=['kivy.deps.gstreamer'],
+
+    # dependency_links=['https://missionpinball.com'],
+
+    tests_require=['mock']
+)
