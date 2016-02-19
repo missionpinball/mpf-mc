cf975fc031db3c4efd796ad71146575b3f87a3ec
toomanybrians <brian@missionpinball.com>
2016-02-19 01:55:55 -0800

    updated setup tag

--- mpf/mc/_version.py
+++ mpf/mc/_version.py
@@ -1,7 +1,7 @@
-__version__ = '0.30.0.dev188'
+__version__ = '0.30.0.dev194'
 __bcp_version__ = '1.0'
 __config_version__ = '3'
-__mpf_version_required__ = '0.30.0.dev433'
+__mpf_version_required__ = '0.30.0.dev474'
 
 version = "MPF-MC v{} (config_version={}, BCP v{}, Requires MPF v{})".format(
     __version__, __config_version__, __bcp_version__, __mpf_version_required__)
