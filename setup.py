706b75b9fcdb7e66df4ab79bbe34eba46df32332
Quinn Capen <qcapen@gmail.com>
2016-03-04 11:32:25 -0700

    Added building of Cython extension modules (right now only audio library) to mpfmc setup.

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 """Mission Pinball Framework Media Controller (mpf-mc) setup.py"""
 
-import platform
+import sys
 import re
 
+from os import environ
+from os.path import join, dirname, exists, isdir
 from setuptools import setup, Extension
+
+
+platform = sys.platform
+
+# Detect 32/64bit for OSX (http://stackoverflow.com/a/1405971/798575)
+if sys.platform == 'darwin':
+    if sys.maxsize > 2 ** 32:
+        osx_arch = 'x86_64'
+    else:
+        osx_arch = 'i386'
+
 
 # Get the version number of mpf-mc and the required version of MPF by reading
 # the file directly. We can't import it because that would import mpf and
@@ -30,27 +43,96 @@ else:
     raise RuntimeError("Unable to find MPF version string in %s." % (
         version_file,))
 
-# figure out the system we're on so we can include the proper binaries
-if platform.system() == 'Darwin':
-    binary_extension = 'so'
-elif platform.system() == 'Windows':
-    binary_extension = 'pyd'
-else:
-    binary_extension = None
 
-if binary_extension:
-    package_data = {'audio_interface':
-        'mpfmc/core/audio/audio_interface.{}'.format(binary_extension)}
-else:
-    package_data = dict()
+def determine_sdl2():
+    flags = {}
+    sdl2_path = environ.get('KIVY_SDL2_PATH', None)
+
+    if not sdl2_path and platform == 'darwin':
+        return flags
+
+    # no pkgconfig info, or we want to use a specific sdl2 path, so perform
+    # manual configuration
+    flags['libraries'] = ['SDL2', 'SDL2_mixer']
+    split_chr = ';' if platform == 'win32' else ':'
+    sdl2_paths = sdl2_path.split(split_chr) if sdl2_path else []
+
+    if not sdl2_paths:
+        sdl_inc = join(dirname(sys.executable), 'include', 'SDL2')
+        if isdir(sdl_inc):
+            sdl2_paths = [sdl_inc]
+        sdl2_paths.extend(['/usr/local/include/SDL2', '/usr/include/SDL2'])
+
+    flags['include_dirs'] = sdl2_paths
+
+    flags['extra_link_args'] = []
+    flags['extra_compile_args'] = []
+    flags['extra_link_args'] += (
+        ['-L' + p for p in sdl2_paths] if sdl2_paths else
+        ['-L/usr/local/lib/'])
+
+    # ensure headers for all the SDL2 and sub libraries are available
+    libs_to_check = ['SDL', 'SDL_mixer']
+    can_compile = True
+    for lib in libs_to_check:
+        found = False
+        for d in flags['include_dirs']:
+            fn = join(d, '{}.h'.format(lib))
+            if exists(fn):
+                found = True
+                print('SDL2: found {} header at {}'.format(lib, fn))
+                break
+
+        if not found:
+            print('SDL2: missing sub library {}'.format(lib))
+            can_compile = False
+
+    if not can_compile:
+        return {}
+
+    return flags
+
+
+# Get the build flags for compiling/building against the SDL2 libraries
+sdl2_flags = determine_sdl2()
+
+
+class CythonExtension(Extension):
+
+    def __init__(self, *args, **kwargs):
+        Extension.__init__(self, *args, **kwargs)
+        self.cython_directives = {
+            'c_string_encoding': 'utf-8',
+            'profile': 'USE_PROFILE' in environ,
+            'embedsignature': 'USE_EMBEDSIGNATURE' in environ}
+        # XXX with pip, setuptools is imported before distutils, and change
+        # our pyx to c, then, cythonize doesn't happen. So force again our
+        # sources
+        self.sources = args[1]
+
+
+ext_modules = [
+    # Custom audio library
+    CythonExtension('mpfmc.core.audio.audio_interface',
+                    ['mpfmc/core/audio/audio_interface.pyx'],
+                    include_dirs=sdl2_flags['include_dirs'],
+                    library_dirs=[join(dirname(sys.executable), 'libs')],
+                    libraries=sdl2_flags['libraries'],
+                    extra_objects=[],
+                    extra_compile_args=['-ggdb', '-O2'],
+                    extra_link_args=[]
+                    )
+]
+
 
 install_requires = ['ruamel.yaml>=0.10,<0.11',
                     'mpf>={}'.format(mpf_version),
                     ]
 
-if platform.system() == 'Windows':
+if platform == 'win32':
     install_requires += ['pypiwin32',
                          'kivy.deps.sdl2',
+                         'kivy.deps.sdl2_dev',
                          'kivy.deps.glew',
                          'kivy.deps.gstreamer_dev',
                          'kivy',
@@ -101,11 +183,16 @@ setup(
 
     keywords='pinball',
 
+    ext_modules=ext_modules,
+
     include_package_data=True,
 
-    package_data=package_data,
+    package_data={'mpfmc': ['core/audio/*.pxd', 'core/audio/*.pxi']},
 
-    packages=['mpfmc'],
+    packages=[
+        'mpfmc',
+        'mpfmc.core.audio'
+    ],
 
     zip_safe=False,
 
