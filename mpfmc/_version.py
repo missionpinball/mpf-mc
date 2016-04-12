9e548ea5c04f25555616bcd44ea965001f2db863
Brian Madden <brian@missionpinball.com>
2016-04-12 10:32:55 -0700

    version bump to kick off new pypi packages

--- mpfmc/_version.py
+++ mpfmc/_version.py
@@ -1,7 +1,7 @@
-__version__ = '0.30.0.dev308'
+__version__ = '0.30.0.dev327'
 __bcp_version__ = '1.0'
 __config_version__ = '4'
-__mpf_version_required__ = '0.30.0.dev889'
+__mpf_version_required__ = '0.30.0.dev897'
 
 version = "MPF-MC v{} (config_version={}, BCP v{}, Requires MPF v{})".format(
     __version__, __config_version__, __bcp_version__, __mpf_version_required__)
