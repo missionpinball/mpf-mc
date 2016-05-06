94deadec38528271a73770d53d2879dca731898d
Quinn Capen <qcapen@gmail.com>
2016-05-05 23:12:17 -0600

    Fixed mode test

--- mpfmc/tests/test_Modes.py
+++ mpfmc/tests/test_Modes.py
@@ -10,7 +10,7 @@ class TestModes(MpfMcTestCase):
 
     def test_mc_mode_start(self):
         # this tests that the mode is read properly from the config too
-        self.send(bcp_command='mode_start', name='mode1')
+        self.send(bcp_command='mode_start', name='mode1', priority=300)
         self.assertTrue(self.mc.modes['mode1'].active)
         self.assertEqual(self.mc.modes['mode1'].priority, 300)
 
