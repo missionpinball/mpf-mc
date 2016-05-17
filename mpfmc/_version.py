127689b420965db919309dbf4a87e0190be43b41
Brian Madden <brian@missionpinball.com>
2016-05-18 00:56:28 +0100

    new version to generate new setup packages

--- mpfmc/_version.py
+++ mpfmc/_version.py
@@ -1,7 +1,7 @@
-__version__ = '0.30.0.dev404'
+__version__ = '0.30.0.dev409'
 __bcp_version__ = '1.0'
 __config_version__ = '4'
-__mpf_version_required__ = '0.30.0.dev1484'
+__mpf_version_required__ = '0.30.0.dev1568'
 
 version = "MPF-MC v{} (config_version={}, BCP v{}, Requires MPF v{})".format(
     __version__, __config_version__, __bcp_version__, __mpf_version_required__)
