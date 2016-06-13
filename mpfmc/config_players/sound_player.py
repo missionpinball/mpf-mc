30f941919f86ea6c01c625f3d3760254eee6075f
Jan Kantert <jan-mpf@kantert.net>
2016-06-14 01:50:30 +0200

    do it properly

--- mpfmc/config_players/sound_player.py
+++ mpfmc/config_players/sound_player.py
@@ -168,7 +168,7 @@ Here are several various examples:
     def clear_context(self, context):
         """Stop all sounds from this context."""
         instance_dict = self._get_instance_dict(context)
-        for sound in instance_dict:
+        for sound in instance_dict.values():
             sound.stop()
         self._reset_instance_dict(context)
 
