0f157d1432438df62fa012c4de4291ac63ebe9d9
Jan Kantert <jan-mpf@kantert.net>
2016-08-03 21:48:55 +0200

    always load dmds and give them a name

--- mpfmc/core/physical_dmd.py
+++ mpfmc/core/physical_dmd.py
@@ -1,4 +1,4 @@
-"""Physical DMD"""
+"""Physical DMD."""
 import struct
 from kivy.clock import Clock
 from kivy.graphics.fbo import Fbo
@@ -10,10 +10,15 @@ from mpfmc.widgets.dmd import Gain
 
 
 class PhysicalDmdBase(object):
+
+    """Base class for DMD devices."""
+
     dmd_name_string = 'Physical DMD'
 
-    def __init__(self, mc, config, fps):
+    def __init__(self, mc, name, config):
+        """Initialise DMD."""
         self.mc = mc
+        self.name = name
 
         self.config = self._get_validated_config(config)
 
@@ -32,7 +37,7 @@ class PhysicalDmdBase(object):
         if self.config['brightness'] != 1.0:
             if not 0.0 <= self.config['brightness'] <= 1.0:
                 raise ValueError("DMD brightness value should be between 0.0 "
-                    "and 1.0. Yours is {}".format(self.config['brightness']))
+                                 "and 1.0. Yours is {}".format(self.config['brightness']))
 
             effect_list.append(Gain(gain=self.config['brightness']))
 
@@ -41,26 +46,19 @@ class PhysicalDmdBase(object):
 
         self.fbo.add(self.effect_widget.canvas)
 
-        self._set_dmd_fps(fps)
+        self._set_dmd_fps()
 
     def _get_validated_config(self, config):
         raise NotImplementedError
 
-    def _set_dmd_fps(self, fps):
+    def _set_dmd_fps(self):
         # fps is the rate that the connected client requested. We'll use the
         # lower of the two
 
-        fps = int(fps)
         mc_fps = self.config['fps']
 
         if mc_fps == 0:
             mc_fps = Clock._max_fps
-
-        if fps < mc_fps:
-            self.log.info("MPF DMD requested frame rate of %sfps, so that will"
-                          "be used instead of the configured setting of %s",
-                          fps, mc_fps)
-            mc_fps = fps
 
         if mc_fps > Clock._max_fps:
             self.mc.log.warning("%s fps is higher than mpf-mc fps. "
@@ -80,6 +78,7 @@ class PhysicalDmdBase(object):
                          PhysicalDmdBase.dmd_name_string, fps)
 
     def tick(self, dt):
+        """Draw image for DMD and send it."""
         del dt
         widget = self.source
         fbo = self.fbo
@@ -113,16 +112,19 @@ class PhysicalDmdBase(object):
             self.send(data)
 
     def send(self, data):
+        """Send data to DMD via BCP."""
         raise NotImplementedError
 
 
 class PhysicalDmd(PhysicalDmdBase):
 
+    """Physical monochrome DMD."""
+
     def _get_validated_config(self, config):
-        return self.mc.config_validator.validate_config('physical_dmd', config)
+        return self.mc.config_validator.validate_config('physical_dmds', config)
 
-    def _convert_to_single_bytes(self, data):
-
+    @classmethod
+    def _convert_to_single_bytes(cls, data):
         new_data = bytearray()
         loops = 0
 
@@ -138,22 +140,25 @@ class PhysicalDmd(PhysicalDmdBase):
         return bytes(new_data)
 
     def send(self, data):
-
+        """Send data to DMD via BCP."""
         data = self._convert_to_single_bytes(data)
 
-        self.mc.bcp_processor.send('dmd_frame', rawbytes=data)
+        self.mc.bcp_processor.send('dmd_frame', rawbytes=data, name=self.name)
 
 
 class PhysicalRgbDmd(PhysicalDmdBase):
 
+    """Physical RGB DMD."""
+
     dmd_name_string = 'Physical RGB DMD'
 
     def _get_validated_config(self, config):
-        return self.mc.config_validator.validate_config('physical_rgb_dmd',
+        return self.mc.config_validator.validate_config('physical_rgb_dmds',
                                                         config)
 
     def send(self, data):
-        self.mc.bcp_processor.send('rgb_dmd_frame', rawbytes=data)
+        """Send data to DMD via BCP."""
+        self.mc.bcp_processor.send('rgb_dmd_frame', rawbytes=data, name=self.name)
 
 
 class FlipVertical(EffectBase):
@@ -161,10 +166,6 @@ class FlipVertical(EffectBase):
 
     def __init__(self):
         super().__init__()
-
-        # todo I have no idea why we need to use the value of 0.32 below. I
-        # thave to test to see if that's always the case or varies depending
-        # on screen size.
 
         self.glsl = '''
 
