f1c155e61490721ca2a7e1d5eec7840f7a00309a
Jan Kantert <jan-mpf@kantert.net>
2016-08-10 23:48:42 +0200

    also consider version branch

--- build_scripts/calculate_mpf_branch.py
+++ build_scripts/calculate_mpf_branch.py
@@ -3,11 +3,16 @@
 import git
 import os
 
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from mpfmc._version import __short_version__
+
 mpf_repo = git.Repo('c:\\projects\\mpf')
 this_mpf_mc_branch = os.environ['APPVEYOR_REPO_BRANCH']
 
 if 'origin/{}'.format(this_mpf_mc_branch) in mpf_repo.refs:
     mpf_branch = this_mpf_mc_branch
+elif 'oritin/{}'.format(__short_version__) in mpf_repo.refs:
+    mpf_branch = __short_version__
 else:
     mpf_branch = 'dev'
 
