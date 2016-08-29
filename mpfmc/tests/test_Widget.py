a617d7a174ed9e610bba19e2fc365724b7fd0930
Jan Kantert <jan-mpf@kantert.net>
2016-08-30 00:40:09 +0200

    move test to new branch

--- mpfmc/tests/test_Widget.py
+++ mpfmc/tests/test_Widget.py
@@ -615,13 +615,34 @@ class TestWidget(MpfMcTestCase):
         self.mc.events.post('widget_4up_red')
         self.advance_time(1)
 
+    def test_widget_with_key(self):
+        self.mc.targets['default'].add_slide(name='slide1')
+        self.mc.targets['default'].show_slide('slide1')
+        self.assertEqual(self.mc.targets['default'].current_slide_name,
+                         'slide1')
+
+        self.assertNotIn('widget2', [x.text for x in self.mc.targets[
+            'default'].current_slide.children[0].children])
+
+        # post the event to add widget2 to the default target, default slide
+        self.mc.events.post('show_christmas_slide_full')
+        self.advance_time()
+
+        self.assertIn('widget2', [x.text for x in self.mc.targets[
+            'default'].current_slide.children[0].children])
+
+        self.mc.events.post('remove_christmas_full')
+        self.advance_time()
+
+        self.assertNotIn('widget2', [x.text for x in self.mc.targets[
+            'default'].current_slide.children[0].children])
+
     def test_updating_mode_widget_by_key(self):
         # create a slide and add some base widgets
         self.mc.targets['default'].add_slide(name='slide1')
         self.mc.targets['default'].show_slide('slide1')
         self.assertEqual(self.mc.targets['default'].current_slide_name,
                          'slide1')
-
         self.mc.events.post('add_widget1_to_current')
         self.advance_time()
 
