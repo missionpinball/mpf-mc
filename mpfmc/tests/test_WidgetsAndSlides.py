a8645d80f9836359bb615069d78c3a6a88f74b27
Jan Kantert <jan-mpf@kantert.net>
2016-09-11 18:34:01 +0200

    move tests from mpf

---
+++ mpfmc/tests/test_WidgetsAndSlides.py
@@ -0,0 +1,27 @@
+from mpfmc.tests.MpfIntegrationTestCase import MpfIntegrationTestCase
+from mpfmc.tests.MpfSlideTestCase import MpfSlideTestCase
+
+
+class TestSlidesAndWidgets(MpfIntegrationTestCase, MpfSlideTestCase):
+
+    def getConfigFile(self):
+        return 'config.yaml'
+
+    def getMachinePath(self):
+        return 'tests/machine_files/widgets_and_slides/'
+
+    def test_widget_on_slide_of_another_mode(self):
+        self.post_event("start_mode1")
+        self.advance_time_and_run()
+        self.post_event("show_widget_mode1_on_slide_mode2")
+        self.advance_time_and_run()
+
+        self.assertSlideNotActive("slide_mode2")
+
+        self.post_event("start_mode2")
+        self.advance_time_and_run()
+        self.post_event("show_slide_mode2")
+        self.advance_time_and_run()
+
+        self.assertSlideOnTop("slide_mode2")
+        self.assertTextInSlide("Widget Mode 1", "slide_mode2")
