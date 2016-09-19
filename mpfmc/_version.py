5d5569d2ce4953dd8192ca81b1eb5c023a3e0464
Quinn Capen <qcapen@gmail.com>
2016-09-19 11:56:44 -0600

    Try appveyor caching

--- mpfmc/_version.py
+++ mpfmc/_version.py
@@ -1,5 +1,5 @@
-__version__ = '0.31.0-dev26'
-__short_version__ = '0.31'
+__version__ = '0.32.0-dev01'
+__short_version__ = '0.32'
 __bcp_version__ = '1.0'
 __config_version__ = '4'
 __mpf_version_required__ = '0.31.0-dev277'
