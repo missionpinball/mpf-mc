c21c315c69d4275f33a91aa14832d4e882625ea4
Brian Madden <brian@missionpinball.com>
2016-09-23 18:41:49 -0500

    Refactored plugin config_player modules
    Move plugin_players (the code that runs in MPF) into their own modules
    that don’t have any mpfmc components in them. Reason was it was too
    easy to accidentally import some mpfmc component that itself imported
    kivy logging and that would break everything. This happened a few times
    in the past, so now the plugin_players are safer.
    
    Fixes MPF issue 550, also resolved an issue with Kivy logging getting
    messed up in mpf-mc.

--- mpfmc/config_players/slide_player.py
+++ mpfmc/config_players/slide_player.py
@@ -1,9 +1,6 @@
 from copy import deepcopy
-
-from mpf.config_players.plugin_player import PluginPlayer
 from mpf.core.config_validator import ConfigValidator
 from mpf.core.events import EventHandlerKey
-from mpf.core.utility_functions import Util
 from mpfmc.core.mc_config_player import McConfigPlayer
 
 
@@ -102,7 +99,8 @@ class McSlidePlayer(McConfigPlayer):
     show_section = 'slides'
     machine_collection_name = 'slides'
 
-    def _add_slide_to_target_when_active(self, target_name, key, slide_name, s, instance_dict, **kwargs):
+    def _add_slide_to_target_when_active(self, target_name, key, slide_name, s,
+                                         instance_dict, **kwargs):
         del kwargs
         target = self.machine.targets[target_name]
         if 'widgets' in s:
@@ -113,15 +111,20 @@ class McSlidePlayer(McConfigPlayer):
         if slide_name not in instance_dict[target_name]:
             instance_dict[target_name][slide_name] = False
 
-    def _delayed_actions(self, target_name, s, slide, instance_dict, full_context):
+    def _delayed_actions(self, target_name, s, slide, instance_dict,
+                         full_context):
         if target_name not in instance_dict:
             instance_dict[target_name] = {}
-        if slide in instance_dict[target_name] and instance_dict[target_name][slide]:
-            self.machine.events.remove_handler_by_key(instance_dict[target_name][slide])
+        if (slide in instance_dict[target_name] and
+                instance_dict[target_name][slide]):
+            self.machine.events.remove_handler_by_key(
+                instance_dict[target_name][slide])
         if s['action'] == "play":
             handler = self.machine.events.add_handler(
-                "display_{}_ready".format(target_name), self._add_slide_to_target_when_active,
-                key=full_context, slide_name=slide, s=s, target_name=target_name, instance_dict=instance_dict)
+                "display_{}_ready".format(target_name),
+                self._add_slide_to_target_when_active,
+                key=full_context, slide_name=slide, s=s,
+                target_name=target_name, instance_dict=instance_dict)
             instance_dict[target_name][slide] = handler
 
     def play(self, settings, context, priority=0, **kwargs):
@@ -215,8 +218,9 @@ class McSlidePlayer(McConfigPlayer):
             validated_config[event]['slides'] = dict()
 
             if isinstance(settings, list):
-                raise AssertionError("Config of slide_player for event {} is broken. "
-                                     "It expects a dict not a list".format(event))
+                raise AssertionError(
+                    "Config of slide_player for event {} is broken. "
+                    "It expects a dict not a list".format(event))
 
             if not isinstance(settings, dict):
                 settings = {settings: dict()}
@@ -245,7 +249,9 @@ class McSlidePlayer(McConfigPlayer):
                     slide_settings = dict()
 
                 elif not isinstance(slide_settings, dict):
-                    raise AssertionError("Expected a dict in slide_player {}:{}.".format(event, slide))
+                    raise AssertionError(
+                        "Expected a dict in slide_player {}:{}.".format(event,
+                                                                        slide))
 
                 for key in slide_settings.keys():
                     if key not in ConfigValidator.config_spec['slide_player']:
@@ -290,158 +296,4 @@ class McSlidePlayer(McConfigPlayer):
         self._reset_instance_dict(context)
 
 
-class MpfSlidePlayer(PluginPlayer):
-    """Base class for part of the slide player which runs as part of MPF.
-
-    Note: This class is loaded by MPF and everything in it is in the context of
-    MPF, not the mpf-mc. MPF finds this instance because the mpf-mc setup.py
-    has the following entry_point configured:
-
-        slide_player=mpfmc.config_players.slide_player:register_with_mpf
-
-    """
-    config_file_section = 'slide_player'
-    show_section = 'slides'
-
-    def _validate_config_item(self, device, device_settings):
-        # device is slide name, device_settings
-        dict_is_widgets = False
-
-        # if settings is list, it's widgets
-        if isinstance(device_settings, list):
-            device_settings = dict(widgets=device_settings)
-
-        # Now check to see if all the settings are valid
-        # slide settings. If not, assume it's a single widget settings.
-        if isinstance(device_settings, dict):
-
-            for key in device_settings.keys():
-                if key not in ConfigValidator.config_spec['slide_player']:
-                    dict_is_widgets = True
-                    break
-
-            if dict_is_widgets:
-                device_settings = dict(widgets=[device_settings])
-        else:
-            raise AssertionError("Settings in slide_player {} have to be dict.".format(device))
-
-        # todo make transition manager validation static and use that here too
-
-        device_settings = self.machine.config_validator.validate_config(
-            'slide_player', device_settings)
-
-        if 'transition' in device_settings:
-            if not isinstance(device_settings['transition'], dict):
-                device_settings['transition'] = dict(
-                    type=device_settings['transition'])
-
-            try:
-                device_settings['transition'] = (
-                    self.machine.config_validator.validate_config(
-                        'transitions:{}'.format(
-                        device_settings['transition']['type']),
-                        device_settings['transition']))
-
-            except KeyError:
-                raise ValueError('transition: section of config requires a'
-                                 ' "type:" setting')
-
-        if 'widgets' in device_settings:
-            device_settings['widgets'] = self.process_widgets(
-                device_settings['widgets'])
-
-        return_dict = dict()
-        return_dict[device] = device_settings
-
-        return return_dict
-
-    def process_widgets(self, config):
-        # config is localized to a specific widget section
-
-        # This method is based on
-        # mpfmc.core.config_collections.widget.Widget.process_config()
-
-        # We can't use that one though because there are lots of other imports
-        # that module does.
-
-        # todo we could move that to a central location
-
-        if isinstance(config, dict):
-            config = [config]
-
-        # Note that we don't reverse the order of the widgets here since
-        # they'll be reversed when they're played
-
-        widget_list = list()
-
-        for widget in config:
-            widget_list.append(self.process_widget(widget))
-
-        return widget_list
-
-    def process_widget(self, config):
-        # config is localized widget settings
-
-        config['_default_settings'] = list(config.keys())
-
-        self.machine.config_validator.validate_config('widgets:{}'.format(
-            config['type']).lower(), config, base_spec='widgets:common')
-
-        if 'animations' in config:
-            config['animations'] = self.process_animations(
-                config['animations'])
-
-        else:
-            config['animations'] = None
-
-        return config
-
-    def process_animations(self, config):
-        # config is localized to the slide's 'animations' section
-
-        for event_name, event_settings in config.items():
-
-            # str means it's a list of named animations
-            if isinstance(event_settings, str):
-                event_settings = Util.string_to_list(event_settings)
-
-            # dict means it's a single set of settings for one animation step
-            elif isinstance(event_settings, dict):
-                event_settings = [event_settings]
-
-            # ultimately we're producing a list of dicts, so build that list
-            # as we iterate
-            new_list = list()
-            for settings in event_settings:
-                new_list.append(self.process_animation(settings))
-
-            config[event_name] = new_list
-
-        return config
-
-    def process_animation(self, config):
-        # config is localized to a single animation's settings within a list
-
-        # str means it's a named animation
-        if isinstance(config, str):
-            config = dict(named_animation=config)
-
-        # dict is settings for an animation
-        elif isinstance(config, dict):
-            self.machine.config_validator.validate_config('widgets:animations',
-                                                          config)
-
-            if len(config['property']) != len(config['value']):
-                raise ValueError('Animation "property" list ({}) is not the '
-                                 'same length as the "end" list ({}).'.
-                                 format(config['property'], config['end']))
-
-        return config
-
-
-player_cls = MpfSlidePlayer
 mc_player_cls = McSlidePlayer
-
-
-def register_with_mpf(machine):
-    return 'slide', MpfSlidePlayer(machine)
