324ca48ad19cd012af4ace71abe9eb93cf2f8599
Quinn Capen <qcapen@gmail.com>
2016-11-21 11:10:46 -0700

    Added playlist player (incomplete)

--- mpfmc/assets/playlist.py
+++ mpfmc/assets/playlist.py
@@ -7,8 +7,10 @@ import uuid
 from enum import Enum, unique
 
 from mpf.core.assets import Asset, AssetPool
+from mpf.core.randomizer import Randomizer
 from mpf.core.utility_functions import Util
 from mpfmc.core.audio.audio_interface import AudioInterface, AudioException
+
 
 class PlaylistAsset(Asset):
     """
@@ -35,7 +37,8 @@ class PlaylistAsset(Asset):
         self.log = logging.getLogger('PlaylistAsset')
         self._track = None
         self._crossfade_time = 0.0
-        self._random = False
+        self._type = "sequence"
+        self._scope = "player"
         self._fade_in_first_sound = False
         self._fade_out_last_sound = False
         self._events_when_played = None
@@ -43,9 +46,8 @@ class PlaylistAsset(Asset):
         self._events_when_sound_played = None
         self._events_when_sound_stopped = None
 
-        self._sounds = list()
-        self._sounds_played = set()
-        self._last_sound = None
+        self._sounds = None
+        self._machine_wide_dict = dict()
 
         # Make sure a legal track name has been specified (unless only one track exists)
         if 'track' not in self.config:
@@ -66,6 +68,12 @@ class PlaylistAsset(Asset):
                                      .format(self.config['track'], name))
 
         self._track = track
+
+        if 'type' in self.config and self.config['type'] is not None:
+            self._type = self.config['type']
+
+        if 'scope' in self.config and self.config['scope'] is not None:
+            self._scope = self.config['scope']
 
         if 'crossfade_time' in self.config and self.config['crossfade_time'] is not None:
             self._crossfade_time = Util.string_to_secs(self.config['crossfade_time'])
@@ -92,26 +100,106 @@ class PlaylistAsset(Asset):
             raise AudioException("A playlist must contain at least one sound. "
                                  "Could not create playlist '{}' asset.".format(name))
 
+        # self.config['sounds'] contains a list of sounds to include in the playlist.  Optionally,
+        # each item in the list can also include an integer weight value delimited by a pipe (|)
+        # character.
+
+        # Build list of weighted sound names
+        playlist_sounds = list()
+        for sound in self.config['sounds']:
+            try:
+                name, weight = sound.split('|')
+                if not weight:
+                    weight = 1
+                else:
+                    weight = int(weight)
+            except ValueError:
+                name = sound
+                weight = 1
+
+            playlist_sounds.append((name, weight))
+
+        self._sounds = playlist_sounds
+
     @property
     def crossfade_time(self):
         """Return the fade out time for the playlist (in seconds)"""
         return self._crossfade_time
 
     @property
-    def random(self):
-        """Return whether or not the playlist order is random"""
-        return self._random
+    def type(self):
+        """Return the playlist type"""
+        return self._type
 
-    def play(self, settings=None):
+    def _get_iterator(self, settings, context, calling_context):
+        """
+        Returns the playlist iterator/randomizer
+        Args:
+            settings:
+            context:
+            calling_context:
+
+        Returns:
+
+        """
+        key = "playlist_{}_{}.{}".format(self.name, context, calling_context)
+        if self._scope == 'player':
+            try:
+                playlist_iterator = self.machine.game.player[key]
+            except KeyError:
+                playlist_iterator = self._create_randomizer()
+                self.machine.game.player[key] = playlist_iterator
+        else:
+            try:
+                playlist_iterator = self._machine_wide_dict[key]
+            except KeyError:
+                playlist_iterator = self._create_randomizer()
+                self._machine_wide_dict[key] = playlist_iterator
+
+        return playlist_iterator
+
+    def _create_randomizer(self):
+        """Create a randomizer to manage the list of sounds in the playlist"""
+        randomizer = Randomizer(self._sounds)
+
+        # Set the randomizer behavior based on the playlist type (random mode)
+        if self._type == 'sequence':
+            randomizer.disable_random = True
+            randomizer.loop = False
+            randomizer.force_different = False
+            randomizer.force_all = True
+        elif self._type == 'random':
+            randomizer.disable_random = False
+            randomizer.loop = False
+            randomizer.force_different = False
+            randomizer.force_all = False
+        elif self._type == 'random_force_next':
+            randomizer.disable_random = False
+            randomizer.loop = False
+            randomizer.force_different = True
+            randomizer.force_all = False
+        elif self._type == 'random_force_all':
+            randomizer.disable_random = False
+            randomizer.loop = False
+            randomizer.force_different = True
+            randomizer.force_all = True
+
+        return randomizer
+
+    def play(self, settings, context, calling_context, **kwargs):
         """
         Start the playlist playing
         Args:
             settings: Optional dictionary of settings to override the default values.
+            context:
+            calling_context:
+            kwargs:
         Returns:
             A sound instance of the first song to play in the playlist
         """
         self.log.debug("Play playlist %s on track %s", self.name, self._track.name)
 
+        playlist_iterator = self._get_randomizer(settings, context, calling_context)
 
         return self._track.play_playlist(self)
 
