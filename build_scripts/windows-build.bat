a6c169696913cda1b818a47f18ae18dd0b7d4d8a
Brian Madden <brian@missionpinball.com>
2016-12-03 18:04:43 -0500

    fix setup.py to include package data

--- build_scripts/windows-build.bat
+++ build_scripts/windows-build.bat
@@ -6,7 +6,7 @@ copy distutils.cfg c:\Python34\Lib\distutils\distutils.cfg
 python -m pip install -i https://pypi.anaconda.org/carlkl/simple mingwpy
 set USE_SDL2=1
 set USE_GSTREAMER=1
-python -m pip install cython docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew kivy.deps.gstreamer kivy.deps.glew_dev kivy.deps.sdl2_dev kivy.deps.gstreamer_dev --extra-index-url https://kivy.org/downloads/packages/simple/ --retries 20 --timeout 60
+python -m pip install cython==0.24.1 docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew kivy.deps.gstreamer kivy.deps.glew_dev kivy.deps.sdl2_dev kivy.deps.gstreamer_dev --extra-index-url https://kivy.org/downloads/packages/simple/ --retries 20 --timeout 60
 
 git clone file:///z/git/mpf-mc c:\mpf-mc
 c:
