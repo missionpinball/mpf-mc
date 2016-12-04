cafd721046876d00a36628ae938e614202367642
Brian Madden <brian@missionpinball.com>
2016-12-04 12:58:03 -0500

    updated mpf version requirement to 0.32.2

--- mpfmc/_version.py
+++ mpfmc/_version.py
@@ -2,7 +2,7 @@ __version__ = '0.32.1'
 __short_version__ = '0.32'
 __bcp_version__ = '1.0'
 __config_version__ = '4'
-__mpf_version_required__ = '0.32.1'
+__mpf_version_required__ = '0.32.2'
 
 version = "MPF-MC v{} (config_version={}, BCP v{}, Requires MPF v{})".format(
     __version__, __config_version__, __bcp_version__, __mpf_version_required__)
