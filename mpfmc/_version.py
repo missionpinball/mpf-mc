aa3aaded468bf38e2cc7e2ec0d247da3dbc0d430
Brian Madden <brian@missionpinball.com>
2017-01-21 10:19:38 -0800

    version rev 0.33.0-dev.2

--- mpfmc/_version.py
+++ mpfmc/_version.py
@@ -1,8 +1,8 @@
-__version__ = '0.33.0-dev.1'
+__version__ = '0.33.0-dev.2'
 __short_version__ = '0.33'
 __bcp_version__ = '1.0'
 __config_version__ = '4'
-__mpf_version_required__ = '0.33.0-dev.0'
+__mpf_version_required__ = '0.33.0-dev.3'
 
 version = "MPF-MC v{} (config_version={}, BCP v{}, Requires MPF v{})".format(
     __version__, __config_version__, __bcp_version__, __mpf_version_required__)
