d5e6f848d86ee3d7a7c1bd8848b62ebdde1d7456
Ryan Richardson <ryananthonyrichardson@gmail.com>
2017-03-15 23:21:52 -0700

    Test positioning named widgets (via widget_player) on DMD

--- mpfmc/tests/test_Dmd.py
+++ mpfmc/tests/test_Dmd.py
@@ -12,6 +12,20 @@ class TestDmd(MpfMcTestCase):
 
         self.assertIn('dmd', self.mc.targets)
 
-        self.mc.events.post('slide1')
+        self.mc.events.post('container_slide')
         self.mc.events.post('dmd_slide')
         self.advance_time(2)
+
+    def test_positioning_named_widgets_on_dmd(self):
+
+        self.assertIn('dmd', self.mc.targets)
+
+        self.mc.events.post('container_slide')
+        self.mc.events.post('position_widget_left')
+        self.advance_time(1)
+        self.mc.events.post('position_widget_right')
+        self.advance_time(1)
+        self.mc.events.post('position_widget_top')
+        self.advance_time(1)
+        self.mc.events.post('position_widget_bottom')
+        self.advance_time(1)
