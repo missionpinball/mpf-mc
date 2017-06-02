daec586723f94555f08ca7ad778a15a9170f2795
Quinn Capen <qcapen@gmail.com>
2017-06-01 20:44:31 -0600

    Added additional bitmap font test

--- mpfmc/tests/test_BitmapFonts.py
+++ mpfmc/tests/test_BitmapFonts.py
@@ -67,6 +67,27 @@ class TestBitmapFonts(MpfMcTestCase):
         self.assertEqual(test_font.bitmap_font.get_characters()[122].rect["x"], 58)
         self.assertEqual(test_font.bitmap_font.get_characters()[122].rect["y"], 273)
 
+        # Variable width font (with text descriptor file)
+        self.assertIn('test_font_2', self.mc.bitmap_fonts)
+        test_font_2 = self.mc.bitmap_fonts['test_font_2']
+        self.assertIsNotNone(test_font_2)
+        self.assertIsNotNone(test_font_2.bitmap_font)
+
+        self.assertEqual(test_font_2.bitmap_font.scale_w, 330)
+        self.assertEqual(test_font_2.bitmap_font.scale_h, 511)
+        self.assertEqual(test_font_2.bitmap_font.line_height, 67)
+        self.assertEqual(test_font_2.bitmap_font.base, 47)
+    
+        self.assertTrue(len(test_font_2.bitmap_font.get_characters()), 80)
+        self.assertEqual(test_font_2.bitmap_font.get_characters()[122].id, 122)
+        self.assertEqual(test_font_2.bitmap_font.get_characters()[122].rect["w"], 35)
+        self.assertEqual(test_font_2.bitmap_font.get_characters()[122].rect["h"], 39)
+        self.assertEqual(test_font_2.bitmap_font.get_characters()[122].xadvance, 30)
+        self.assertEqual(test_font_2.bitmap_font.get_characters()[122].xoffset, 1)
+        self.assertEqual(test_font_2.bitmap_font.get_characters()[122].yoffset, 16)
+        self.assertEqual(test_font_2.bitmap_font.get_characters()[122].rect["x"], 80)
+        self.assertEqual(test_font_2.bitmap_font.get_characters()[122].rect["y"], 438)
+    
     def test_bitmap_font_text(self):
         # Very basic test
         self.mc.events.post('static_text')
