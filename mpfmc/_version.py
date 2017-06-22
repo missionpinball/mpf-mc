70a97d81660bd48b013f150cfd44474186e710fc
Quinn Capen <qcapen@gmail.com>
2017-06-22 15:03:54 -0600

    Release 0.33.9

--- mpfmc/_version.py
+++ mpfmc/_version.py
@@ -1,8 +1,8 @@
-__version__ = '0.33.8'
+__version__ = '0.33.9'
 __short_version__ = '0.33'
 __bcp_version__ = '1.1'
 __config_version__ = '4'
-__mpf_version_required__ = '0.33.9'
+__mpf_version_required__ = '0.33.29'
 
 version = "MPF-MC v{} (config_version={}, BCP v{}, Requires MPF v{})".format(
     __version__, __config_version__, __bcp_version__, __mpf_version_required__)
