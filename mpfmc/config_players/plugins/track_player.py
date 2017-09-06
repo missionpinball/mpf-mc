8b01997ab8353db2776e8890d7ca6ead158bb13b
Quinn Capen <qcapen@gmail.com>
2017-09-06 09:16:32 -0600

    Fix track_player bug
    track_player plugin was not defined in the plugins directory where it should have been.

---
+++ mpfmc/config_players/plugins/track_player.py
@@ -0,0 +1,22 @@
+from mpf.config_players.plugin_player import PluginPlayer
+
+
+class MpfTrackPlayer(PluginPlayer):
+    """Base class for part of the track player which runs as part of MPF.
+
+    Note: This class is loaded by MPF and everything in it is in the context of
+    MPF, not the mpf-mc. MPF finds this instance because the mpf-mc setup.py
+    has the following entry_point configured:
+
+        track_player=mpfmc.config_players.track_player:register_with_mpf
+
+    """
+    config_file_section = 'track_player'
+    show_section = 'tracks'
+
+player_cls = MpfTrackPlayer
+
+
+def register_with_mpf(machine):
+    """Registers the sound player plug-in with MPF"""
+    return 'track', MpfTrackPlayer(machine)
--- mpfmc/config_players/track_player.py
+++ mpfmc/config_players/track_player.py
@@ -4,7 +4,6 @@
 # load in the mpf process when MPF processes the MpfSoundPlayer
 from copy import deepcopy
 
-from mpf.config_players.plugin_player import PluginPlayer
 from mpf.core.config_validator import ConfigValidator
 from mpfmc.core.mc_config_player import McConfigPlayer
 
@@ -189,24 +188,4 @@ Here are several various examples:
         track_player."""
         del context
 
-
-class MpfTrackPlayer(PluginPlayer):
-    """Base class for part of the track player which runs as part of MPF.
-
-    Note: This class is loaded by MPF and everything in it is in the context of
-    MPF, not the mpf-mc. MPF finds this instance because the mpf-mc setup.py
-    has the following entry_point configured:
-
-        track_player=mpfmc.config_players.track_player:register_with_mpf
-
-    """
-    config_file_section = 'track_player'
-    show_section = 'tracks'
-
-player_cls = MpfTrackPlayer
 mc_player_cls = McTrackPlayer
-
-
-def register_with_mpf(machine):
-    """Registers the sound player plug-in with MPF"""
-    return 'track', MpfTrackPlayer(machine)
