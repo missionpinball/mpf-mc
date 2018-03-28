5304028dbb8835660cb385ec274796129abe1269
Jan Kantert <jan-mpf@kantert.net>
2018-03-29 01:06:50 +0200

    remove debug output

--- mpfmc/core/mc.py
+++ mpfmc/core/mc.py
@@ -11,7 +11,6 @@ import asyncio
 
 import gc
 import weakref
-from collections import defaultdict
 
 from kivy.app import App
 from kivy.clock import Clock
@@ -132,7 +131,6 @@ class MpfMc(App):
         self.ticks = 0
         self.start_time = 0
         self.debug_refs = []
-        self.object_stats = defaultdict(int)
 
         if thread_stopper:
             self.thread_stopper = thread_stopper
@@ -437,14 +435,6 @@ class MpfMc(App):
             self.log.info(ev)
             ev = ev.next
         self.log.info("--- DEBUG DUMP CLOCK END ---")
-        gc.collect()
-        current_obj = defaultdict(int)
-        for i in gc.get_objects():
-            current_obj[type(i)] += 1
-
-        self.log.info([(k, current_obj[k] - self.object_stats[k]) for k in current_obj
-                       if current_obj[k] - self.object_stats[k]])
-        self.object_stats = current_obj
 
     def on_stop(self):
         self.log.info("Stopping...")
