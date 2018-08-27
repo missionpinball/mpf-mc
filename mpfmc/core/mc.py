71258812c1eb1b5715477ae4e9578f03e5844d91
Jan Kantert <jan-mpf@kantert.net>
2018-08-27 22:47:12 +0200

    fix the fix

--- mpfmc/core/mc.py
+++ mpfmc/core/mc.py
@@ -537,7 +537,7 @@ class MpfMc(App):
         if value is None:
             try:
                 del self.machine_vars[name]
-            except IndexError:
+            except KeyError:
                 pass
         else:
             self.machine_vars[name] = value
