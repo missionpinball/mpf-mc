405ce386f5eb10ff10419e30b6e1d205bbfcbac6
Jan Kantert <jan-mpf@kantert.net>
2018-11-25 14:02:56 +0100

    pin to old kivy dependencies

--- setup.py
+++ setup.py
@@ -578,10 +578,10 @@ install_requires = ['ruamel.yaml>=0.10,<0.11',  # better YAML library
                     'pygments',  # YAML syntax formatting for the iMC
                     'pypiwin32>=223;platform_system=="Windows" and python_version>"3.4"',
                     'pypiwin32<=219;platform_system=="Windows" and python_version=="3.4"',
-                    'kivy.deps.sdl2>=0.1.17;platform_system=="Windows"',
-                    'kivy.deps.sdl2_dev>=0.1.17;platform_system=="Windows"',
-                    'kivy.deps.glew>=0.1.9;platform_system=="Windows"',
-                    'kivy.deps.gstreamer>=0.1.12;platform_system=="Windows"',
+                    'kivy.deps.sdl2==0.1.17;platform_system=="Windows"',
+                    'kivy.deps.sdl2_dev==0.1.17;platform_system=="Windows"',
+                    'kivy.deps.glew==0.1.9;platform_system=="Windows"',
+                    'kivy.deps.gstreamer==0.1.12;platform_system=="Windows"',
                     ]
 
 # If we're running on Read The Docs, then we just need to copy the files
