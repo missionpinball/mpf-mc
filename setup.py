ce1ba3c602e5821c9c518bfde6bb7994ef5cc9bb
Jan Kantert <jan-mpf@kantert.net>
2019-01-21 18:39:55 +0100

    changes to use kivy app

--- setup.py
+++ setup.py
@@ -573,7 +573,7 @@ else:
 
 install_requires = ['ruamel.yaml>=0.10,<0.11',  # better YAML library
                     'mpf>={}'.format(mpf_version),
-                    'kivy>=1.10.1',
+                    'kivy>=1.10.1;platform_system!="Mac"',
                     'psutil',
                     'pygments',  # YAML syntax formatting for the iMC
                     'pypiwin32>=223;platform_system=="Windows" and python_version>"3.4"',
