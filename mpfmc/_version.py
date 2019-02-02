f34c3870d5e3a9ecf64331b75ad8e2c4cea9488d
Jan Kantert <jan-mpf@kantert.net>
2019-02-02 16:11:55 +0100

    new dev version 0.53.0-dev.0

--- mpfmc/_version.py
+++ mpfmc/_version.py
@@ -1,8 +1,8 @@
-__version__ = '0.52.0-dev.2'
-__short_version__ = '0.52'
+__version__ = '0.53.0-dev.0'
+__short_version__ = '0.53'
 __bcp_version__ = '1.1'
 __config_version__ = '5'
-__mpf_version_required__ = '0.52.0-dev.0'
+__mpf_version_required__ = '0.53.0-dev.0'
 
 version = "MPF-MC v{} (config_version={}, BCP v{}, Requires MPF v{})".format(
     __version__, __config_version__, __bcp_version__, __mpf_version_required__)
