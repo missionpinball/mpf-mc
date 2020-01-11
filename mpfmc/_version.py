7d17cd74ece3393c348d02aba588b590b1e806e4
Jan Kantert <jan-mpf@kantert.net>
2020-01-11 16:13:57 +0100

    release 0.53.0

--- mpfmc/_version.py
+++ mpfmc/_version.py
@@ -1,8 +1,8 @@
-__version__ = '0.53.0-dev.16'
+__version__ = '0.53.0'
 __short_version__ = '0.53'
 __bcp_version__ = '1.1'
 __config_version__ = '5'
-__mpf_version_required__ = '0.53.0-dev.51'
+__mpf_version_required__ = '0.53.0'
 
 # pylint: disable-msg=invalid-name
 version = "MPF-MC v{}".format(__version__)
