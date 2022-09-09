630673412e621f7de11bac2f797f648ea39ab8d1
Brian Madden <brian@fastpinball.com>
2022-09-09 14:38:35 -0700

    update version to 0.56.0.dev34

--- mpfmc/_version.py
+++ mpfmc/_version.py
@@ -1,13 +1,13 @@
-__version__ = '0.56.0-dev33'
+__version__ = '0.56.0-dev34'
 __short_version__ = '0.56'
 __bcp_version__ = '1.1'
 __config_version__ = '5'
 __mpf_version_required__ = '0.56.0-dev33'
 
 # pylint: disable-msg=invalid-name
-version = "MPF-MC v{}".format(__version__)
+version = f"MPF-MC v{__version__}"
 '''A friendly version string for this build of MPF.'''
 
 # pylint: disable-msg=invalid-name
-extended_version = "MPF-MC v{} (config_version={}, BCP v{}, Requires MPF v{})".format(
-    __version__, __config_version__, __bcp_version__, __mpf_version_required__)
+extended_version = f"MPF-MC v{__version__} (config_version={__config_version__}, BCP v{__bcp_version__}, \
+                     Requires MPF API compatibility v{ __mpf_version_required__} or newer)"
