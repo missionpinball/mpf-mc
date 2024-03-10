dec18082f89c93c3617c0830f726850d811970fc
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-03-10 16:19:51 -0700

    Version release 0.57.0

--- mpfmc/_version.py
+++ mpfmc/_version.py
@@ -1,10 +1,10 @@
 import os
 
-__version__ = '0.57.0.dev14'
+__version__ = '0.57.0'
 __short_version__ = '0.57'
 __bcp_version__ = '1.1'
 __config_version__ = '6'
-__mpf_version_required__ = '0.57.0.dev40'  # ALSO UPDATE in pyproject.toml
+__mpf_version_required__ = '0.57.0'  # ALSO UPDATE in pyproject.toml
 
 # pylint: disable-msg=invalid-name
 version = f"MPF-MC v{__version__}"
